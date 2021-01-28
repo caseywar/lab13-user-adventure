e713ac94568aca77d5152de9c6b9d882b96b63f8
Casey Warren <caseywar1017@gmail.com>
2021-01-28 18:29:12 -0500

    passing test for Form Data

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
+import { setUser } from '../utils.js';
+
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const formData = new FormData(form);
-
-    const user = {
-        name: formData.get('name'),
-        element: formData.get('element'),
-        jp: 0,
-        cp: 0,
-        completed: {},
-    };
-
-    const stringyUser = JSON.stringify(user);
-    localStorage.setItem('USER', stringyUser);
-
+    console.log(formData);
+    setUser(formData);
     window.location = './map';
 });
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,25 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { setUser } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+test('the FormData should save user input to local storage', (expect) => {
+    const form = new FormData();
+    form.append('name', 'case');
+    form.append('element', 'water');
+    setUser(form);
+
+    const actual = localStorage.getItem('USER');
+    const trueActual = JSON.parse(actual);
+
+    const expected = {
+        name: 'case',
+        cp: 0,
+        element: 'water',
+        jp: 0,
+        completed: {},
+    };
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.deepEqual(trueActual, expected);
 });
