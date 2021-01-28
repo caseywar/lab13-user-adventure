d82b1dd3085f8cbbb572463d0645ee3ff4df032f
Casey Warren <caseywar1017@gmail.com>
2021-01-28 01:05:29 -0500

    working on creating elements and appending them in quest js

--- quest/quest.js
+++ quest/quest.js
@@ -12,6 +12,47 @@ const questId = params.get('id');
 
 const quest = findById(quests, questId);
 
-console.log('=============================\n');
-console.log('|| quest', quest);
-console.log('\n=============================');
+h1.textContent = quest.title;
+p.textContent = quest.description;
+img.src = `../assets/${quest.image}`;
+
+for (let choice of quest.choices) {
+    const radio = document.createElement('input');
+    const label = document.createElement('label');
+    const span = document.createElement('span');
+
+    span.textContent = choice.description;
+
+    radio.type = 'radio';
+    radio.value = choice.id;
+    radio.name = 'choices';
+
+    label.append(span, radio);
+
+    form.append(label);
+}
+
+const button = document.createElement('button');
+button.textContent = 'Submit';
+
+form.appendChild(button);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const selectionId = formData.get('choices');
+
+    const choice = findById(quest.choices, selectionId);
+    const user = JSON.parse(localStorage.getItem('USER'));
+
+    console.log('=============================\n');
+    console.log('|| quest', quest);
+    console.log('\n=============================');
+
+    user.jp += choice.jp;
+
+    localStorage.setItem('USER', JSON.stringify(user));
+
+    window.location = '../map';
+});
